967cae46af26f00cc79c65b26e3e867ca6349e25
unknown <daumisgod@gmail.com>
2017-01-18 18:39:43 -0500

    played with lab1

--- PracticeLab1/app/src/main/java/com/example/mdaum/practicelab1/MainActivity.java
+++ PracticeLab1/app/src/main/java/com/example/mdaum/practicelab1/MainActivity.java
@@ -2,12 +2,18 @@ package com.example.mdaum.practicelab1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        int x=0;
         super.onCreate(savedInstanceState);
+        x=1;
         setContentView(R.layout.activity_main);
+        x=2;
+        Log.v("Debug_Tag","test debug");
+        x=3;
     }
 }
